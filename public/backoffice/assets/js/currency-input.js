aae51357c3675676d9fc18b3f6ad731a45157f11
Jorge Reis <jorgew.reis@outlook.com>
2026-09-02 18:24:40 -0300

    Avoid duplicate currency prefix in backoffice inputs

--- public/backoffice/assets/js/currency-input.js
+++ public/backoffice/assets/js/currency-input.js
@@ -2,8 +2,6 @@
     "use strict";
 
     const formatter = new Intl.NumberFormat("pt-BR", {
-        style: "currency",
-        currency: "BRL",
         minimumFractionDigits: 2,
         maximumFractionDigits: 2,
     });
