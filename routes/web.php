44a352256990a0c6defbf4bb13a24228385cf6a6
Jorge Reis <jorgew.reis@outlook.com>
2026-09-09 13:08:39 -0300

    feat: add Fokus Styles documentation mockups

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::domain('styles.fokuscloud.com.br')->group(function () {
+    Route::get('/', function () {
+        return response()->file(base_path('mockups/pages/styles-model-6.html'));
+    });
+});
+
 Route::get('/', function () {
     return response()->file(base_path('mockups/pages/index.html'));
 });
